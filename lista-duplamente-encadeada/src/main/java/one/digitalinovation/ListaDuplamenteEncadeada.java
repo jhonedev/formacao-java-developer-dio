f1c20d4fead819afadded870e772b97c8df468c9
Jonathan <jonatan.borges2012@hotmail.com>
2024-11-27 16:12:30 -0300

    Implementação dos métodos get e getNo

--- lista-duplamente-encadeada/src/main/java/one/digitalinovation/ListaDuplamenteEncadeada.java
+++ lista-duplamente-encadeada/src/main/java/one/digitalinovation/ListaDuplamenteEncadeada.java
@@ -12,6 +12,19 @@ public class ListaDuplamenteEncadeada<T> {
         this.tamanhoLista = 0;
     }
 
+    public T get(int index){
+        return getNo(index).getConteudo();
+    }
+
+    private NoDuplo<T> getNo(int index) {
+        NoDuplo<T> noAuxiliar = primeiroNo;
+
+        for (int i = 0; (i < index) && (noAuxiliar != null); i++) {
+            noAuxiliar = noAuxiliar.getNoProximo();
+        }
+        return noAuxiliar;
+    }
+
     public int size() {
         return tamanhoLista;
     }
