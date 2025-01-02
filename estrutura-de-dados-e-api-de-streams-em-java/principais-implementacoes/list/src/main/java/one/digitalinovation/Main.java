78483f7679c29722b2e69654ba67e231ba556052
Jonathan <jonatan.borges2012@hotmail.com>
2025-01-02 19:49:59 -0300

    Implementando a classe Main e testando

--- estrutura-de-dados-e-api-de-streams-em-java/principais-implementacoes/list/src/main/java/one/digitalinovation/Main.java
+++ estrutura-de-dados-e-api-de-streams-em-java/principais-implementacoes/list/src/main/java/one/digitalinovation/Main.java
@@ -1,33 +1,27 @@
 package one.digitalinovation;
 
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        Queue<Carro> queueCarros = new LinkedList<>();
+        List<Carro> listCarros = new ArrayList<>();
 
-        queueCarros.add(new Carro("Ford"));
-        queueCarros.add(new Carro("Chevrolet"));
-        queueCarros.add(new Carro("Fiat"));
+        listCarros.add(new Carro("Fiat"));
+        listCarros.add(new Carro("Peugeut"));
+        listCarros.add(new Carro("Chevrolet"));
+        listCarros.add(new Carro("Uno"));
 
-        System.out.println(queueCarros.add(new Carro("Peugot"))); // adiciona a lista, caso nao consiga, retorna um erro.
-        System.out.println(queueCarros);
+        System.out.println(listCarros.contains(new Carro("Fiat")));
 
-        System.out.println(queueCarros.offer(new Carro("Renault"))); // ao contrario do add, ele adiciona, mas se não conseguir add ele retorna false
-        System.out.println(queueCarros);
+        System.out.println(listCarros.get(3));
 
-        System.out.println(queueCarros.peek()); // verifica quem é o item que está no inicio da fila, sem excluir
-        System.out.println(queueCarros);
+        System.out.println(listCarros.indexOf(new Carro("Fiat"))); // informa em qual posicao (indice) está o item selecionado
 
-        System.out.println(queueCarros.poll()); // verifica quem é o item que está no inicio da fila e exclui.
-        System.out.println(queueCarros);
-
-        System.out.println(queueCarros.isEmpty()); // verifica se a fila está vazia
-
-        System.out.println(queueCarros.size()); // verifica o tamanho da fila
+        System.out.println(listCarros.remove(2));
+        System.out.println(listCarros);
 
 
     }
