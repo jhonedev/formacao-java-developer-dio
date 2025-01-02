ed0fdc8c88b9272de404450c0e60a9e437a130a7
Jonathan <jonatan.borges2012@hotmail.com>
2025-01-02 18:59:43 -0300

    Implementação das classes Carro e Main

---
+++ estrutura-de-dados-e-api-de-streams-em-java/queue/src/main/java/one/digitalinovation/Carro.java
@@ -0,0 +1,38 @@
+package one.digitalinovation;
+
+import java.util.Objects;
+
+public class Carro {
+    String marca;
+
+    public Carro(String marca) {
+        this.marca = marca;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Carro carro = (Carro) o;
+        return Objects.equals(marca, carro.marca);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(marca);
+    }
+
+    @Override
+    public String toString() {
+        return "Carro{" +
+                "marca='" + marca + '\'' +
+                '}';
+    }
+}
---
+++ estrutura-de-dados-e-api-de-streams-em-java/queue/src/main/java/one/digitalinovation/Main.java
@@ -0,0 +1,17 @@
+package one.digitalinovation;
+
+
+import java.util.Stack;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        
+
+
+
+
+    }
+
+}
