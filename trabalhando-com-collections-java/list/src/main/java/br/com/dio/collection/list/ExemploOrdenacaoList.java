245f4c33e0ae83974d84d5b122bfa3feda4acaea
Jonathan <jonatan.borges2012@hotmail.com>
2025-01-06 11:52:18 -0300

    Implementação da classe Gato e ExemploOrdenacaoList e resolvendo alguns exercicios

---
+++ trabalhando-com-collections-java/list/src/main/java/br/com/dio/collection/list/ExemploOrdenacaoList.java
@@ -0,0 +1,34 @@
+package br.com.dio.collection.list;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/*
+        Dados os seguintes informaçoes sobre os gatos, crei uma lista e ordene esta lista exibindo:
+        nome - idade - cor
+        Gato 1 - nome: Jon, idade: 18, cor: preto
+        Gato 2 - nome: Simba, idade: 6, cor: tigrado
+        Gato 3 - nome: Jon, idade: 12, cor: amarelo*/
+public class ExemploOrdenacaoList {
+    public static void main(String[] args) {
+        List<Gato> gatos = new ArrayList<>();
+
+        System.out.println("Ordem de inserção: ");
+        gatos.add(new Gato("Jon", 18, "preto"));
+        gatos.add(new Gato("Simba", 6, "tigrado"));
+        gatos.add(new Gato("Jon", 12, "amarelo"));
+        System.out.println(gatos);
+
+        System.out.println("Ordem aleatória");
+        Collections.shuffle(gatos);
+        System.out.println(gatos);
+
+        System.out.println("Ordem natural (Nome)");
+        Collections.sort(gatos);
+
+        System.out.println("Ordem Idade");
+
+
+    }
+}
