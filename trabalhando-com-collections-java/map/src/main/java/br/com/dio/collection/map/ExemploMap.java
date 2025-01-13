16cc04a48b48c275af05ad780bae4d66d36b8022
Jonathan <jonatan.borges2012@hotmail.com>
2025-01-12 23:16:43 -0300

    Resolvendo o restante dos exercicios, parte final

--- trabalhando-com-collections-java/map/src/main/java/br/com/dio/collection/map/ExemploMap.java
+++ trabalhando-com-collections-java/map/src/main/java/br/com/dio/collection/map/ExemploMap.java
@@ -66,6 +66,48 @@ public class ExemploMap {
             }
         }
 
+        System.out.println("Exiba a soma dos valores");
+        Iterator<Double> iterator = modeloCarros.values().iterator();
+        Double soma = 0d;
+
+        while (iterator.hasNext()){
+            soma += iterator.next();
+        }
+        System.out.println("A soma dos valores foi de : " + soma);
+
+        System.out.println("Exiba a media dos consumos deste dicionario de carros");
+        Double media = soma / modeloCarros.size();
+        System.out.println(media);
+
+        System.out.println("Remova os modelos com o consumo igual a 15.6 km/l");
+        Iterator<Double> iterator1 = modeloCarros.values().iterator();
+        while (iterator1.hasNext()){
+            if (iterator1.next().equals(15.6)){
+                iterator1.remove();
+            }
+        }
+        System.out.println(modeloCarros);
+
+        System.out.println("Exiba todos os carros na ordem em que foram informado");
+        Map<String, Double> modeloCarros1 = new LinkedHashMap<>();
+        modeloCarros1.put("Gol", 14.4);
+        modeloCarros1.put("Uno", 15.6);
+        modeloCarros1.put("Mobi", 16.1);
+        modeloCarros1.put("Hb20", 14.5);
+        modeloCarros1.put("Kwid", 15.6);
+
+        System.out.println(modeloCarros1);
+
+        System.out.println("Exiba o dicionario ordenado pelo modelo (ordem alfabetica)");
+        Map<String, Double> modeloCarros2 = new TreeMap<>(modeloCarros1);
+        System.out.println(modeloCarros2);
+
+        System.out.println("Apague o dicionario de carros");
+        modeloCarros1.clear();
+        System.out.println(modeloCarros1);
+
+        System.out.println("Confir se a lista esta vazia");
+        System.out.println(modeloCarros1.isEmpty());
 
 
     }
