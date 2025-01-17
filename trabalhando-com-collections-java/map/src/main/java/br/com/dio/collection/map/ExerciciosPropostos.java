e5db056a1bfcaa0f900c22e91aa6244c55018e0e
Jonathan <jonatan.borges2012@hotmail.com>
2025-01-17 15:50:50 -0300

    Finalizando os primeiros exercicios propostos

--- trabalhando-com-collections-java/map/src/main/java/br/com/dio/collection/map/ExerciciosPropostos.java
+++ trabalhando-com-collections-java/map/src/main/java/br/com/dio/collection/map/ExerciciosPropostos.java
@@ -22,7 +22,6 @@ public class ExerciciosPropostos {
         estados.put("RN", new Estado(3534265));
 
         Set<Map.Entry<String, Estado>> estadoEntry = estados.entrySet();
-
         for (Map.Entry<String, Estado> stringEstadoEntry : estadoEntry) {
             System.out.println(stringEstadoEntry.getKey() + " - " + stringEstadoEntry.getValue().getPopulacao());
         }
@@ -45,5 +44,86 @@ public class ExerciciosPropostos {
 
         System.out.println("Exiba a populaao de PE");
         System.out.println(estados.get("PE").getPopulacao());
+
+        System.out.println("Esiba todos os estados na ordem em que foram inseridos");
+        Map<String, Estado> estados1 = new LinkedHashMap<>();
+        estados1.put("PE", new Estado(9616621));
+        estados1.put("AL", new Estado(3351543));
+        estados1.put("CE", new Estado(9187103));
+        estados1.put("RN", new Estado(3534265));
+
+        Set<Map.Entry<String, Estado>> entrySet = estados1.entrySet();
+        for (Map.Entry<String, Estado> stringEstadoEntry : entrySet) {
+            System.out.println(stringEstadoEntry.getKey() + " - " + stringEstadoEntry.getValue().getPopulacao());
+        }
+
+        System.out.println("Exiba os estados e suas populacoes em ordem alfabetica");
+        Map<String, Estado> estadoMap = new TreeMap<>(estados);
+        Set<Map.Entry<String, Estado>> entrySet1 = estadoMap.entrySet();
+        for (Map.Entry<String, Estado> stringEstadoEntry : entrySet1) {
+            System.out.println(stringEstadoEntry.getKey() + " - " + stringEstadoEntry.getValue().getPopulacao());
+        }
+
+        System.out.println("Exiba o estado com a maior e menor populacao e sua quantidade");
+        Map<String, Integer> estados2 = new HashMap<>();
+
+        estados2.put("PE", 9616621);
+        estados2.put("AL", 3351543);
+        estados2.put("CE", 9187103);
+        estados2.put("RN", 3534265);
+
+        Collection<Integer> populacao = estados2.values();
+        String EstadoMaiorPopulacao = "";
+        String EstadoMenorPopulacao = "";
+        Integer min = Collections.min(populacao);
+        Integer max = Collections.max(populacao);
+
+        for (Map.Entry<String, Integer> entry : estados2.entrySet()) {
+            if (entry.getValue().equals(Collections.max(populacao))){
+                EstadoMaiorPopulacao = entry.getKey();
+            }
+            if (entry.getValue().equals(Collections.min(populacao))){
+                EstadoMenorPopulacao = entry.getKey();
+            }
+        }
+        System.out.println("Estado com maior populacao: " + EstadoMaiorPopulacao + " com " + max);
+        System.out.println("Estado com menor populacao: " + EstadoMenorPopulacao + " com " + min);
+
+        System.out.println("Exiba a soma da populacao desses estados");
+        int soma = 0;
+        for (int i : populacao) {
+            soma += i;
+        }
+        // ou
+
+//        Iterator<Integer> iterator = estados2.values().iterator();
+//        int soma1 = 0;
+//        while (iterator.hasNext()) {
+//            soma1 += iterator.next();
+//        }
+        System.out.println("A soma da populacao é : " + soma);
+
+        System.out.println("Exiba a media da populacao desse dicionario");
+        double media = (double) soma / populacao.size();
+        System.out.println("A media da populacao é " + media);
+
+        System.out.println("Remova os estados com populacao menor que 4000000");
+        Iterator<Map.Entry<String, Integer>> entries = estados2.entrySet().iterator();
+        while (entries.hasNext()){
+            if (entries.next().getValue() < 4000000){
+                entries.remove();
+            }
+        }
+
+        for (Map.Entry<String, Integer> entry : estados2.entrySet()) {
+            System.out.println(entry.getKey() + " - " + entry.getValue());
+        }
+
+        System.out.println("Apague o dicionario de estados");
+        estados.clear();
+
+        System.out.println("Confira se o dicionario esta vazio");
+        System.out.println(estados);
+
     }
 }
