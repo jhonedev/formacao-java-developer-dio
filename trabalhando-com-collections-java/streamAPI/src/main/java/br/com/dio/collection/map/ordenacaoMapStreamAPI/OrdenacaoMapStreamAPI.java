80d886e914be45d123ce48cb78356a2eae69760b
Jonathan <jonatan.borges2012@hotmail.com>
2025-01-18 11:47:06 -0300

    Refatorando o exercicio ordem nome contato com a expressao lambda

--- trabalhando-com-collections-java/streamAPI/src/main/java/br/com/dio/collection/map/ordenacaoMapStreamAPI/OrdenacaoMapStreamAPI.java
+++ trabalhando-com-collections-java/streamAPI/src/main/java/br/com/dio/collection/map/ordenacaoMapStreamAPI/OrdenacaoMapStreamAPI.java
@@ -68,7 +68,8 @@ public class OrdenacaoMapStreamAPI {
 
         System.out.println("--\tOrdem nome contato\t--");
         //precisamos organizar os valores. Logo:
-        Set<Map.Entry<Integer, Contato>> set1 = new TreeSet<>(new ComparatorOrdemNomeContato());
+        Set<Map.Entry<Integer, Contato>> set1 = new TreeSet<>(Comparator.comparing(
+                cont -> cont.getValue().getNome()));
         set1.addAll(agenda.entrySet());
         for (Map.Entry<Integer, Contato> entry: set1) {
             System.out.println(entry.getKey() + " - " + entry.getValue().getNome());
