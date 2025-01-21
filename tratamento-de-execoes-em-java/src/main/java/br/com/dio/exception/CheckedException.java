4f0319f863a7a50f21b6c96c797e310c18c6428e
Jonathan <jonatan.borges2012@hotmail.com>
2025-01-21 15:39:52 -0300

    Finalizando a parte 2 do tratamento de exececao Checked exception

--- tratamento-de-execoes-em-java/src/main/java/br/com/dio/exception/CheckedException.java
+++ tratamento-de-execoes-em-java/src/main/java/br/com/dio/exception/CheckedException.java
@@ -1,13 +1,26 @@
 package br.com.dio.exception;
 
+import javax.swing.*;
 import java.io.*;
 
 public class CheckedException {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
 
         // Imprimir um arquivo no console
         String nomeArquivo = "romances-blake-crouch.txt";
-        imprimirArquivoConsole(nomeArquivo);
+        try {
+            imprimirArquivoConsole(nomeArquivo);
+        }catch (FileNotFoundException e) {
+            JOptionPane.showMessageDialog(null,
+                    "Revise o nome do seu arquivo que você deseja imprimir");
+            e.printStackTrace();
+        } catch (IOException e) {
+            JOptionPane.showMessageDialog(null,
+                    "Ocorreu um erro inesperado, entre em contato com o suporte!");
+            e.printStackTrace();
+        }finally {
+            System.out.println("Chegou no finally");
+        }
 
         System.out.println("Apesar da exception ou nao, o programa continua");
 
