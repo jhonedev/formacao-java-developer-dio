2505f0d0f4b1ac14d035df7cd919815a221700e6
Jonathan <jonatan.borges2012@hotmail.com>
2025-01-20 21:58:52 -0300

    Resolvendo o exemplo de excecao Unchecked

---
+++ tratamento-de-execoes-em-java/src/main/java/br/com/dio/exception/Dividir.java
@@ -0,0 +1,7 @@
+package br.com.dio.exception;
+
+public class Dividir {
+    public int dividir(int num1, int num2){
+        return num1 / num2;
+    }
+}
---
+++ tratamento-de-execoes-em-java/src/main/java/br/com/dio/exception/UncheckedExeption.java
@@ -0,0 +1,36 @@
+package br.com.dio.exception;
+
+import javax.swing.*;
+
+public class UncheckedExeption {
+    public static void main(String[] args) {
+
+        boolean continuaLooping = true;
+        do {
+            //Fazer a divisao de dois numero inteiros
+            String a  = JOptionPane.showInputDialog("Numerador");
+            String b =  JOptionPane.showInputDialog("Denominador");
+
+            try {
+                int num1 = Integer.parseInt(a);
+                int num2 = Integer.parseInt(b);
+
+                Dividir calcular = new Dividir();
+                int resultado = calcular.dividir(num1, num2);
+                System.out.println(resultado);
+                continuaLooping = false;
+            } catch (NumberFormatException exception) {
+                JOptionPane.showMessageDialog(null, "Entrada invalida, infome numeros inteiro");
+                //e.printStackTrace();
+            }catch (ArithmeticException exception){
+                JOptionPane.showMessageDialog(null, "Impossivel dividir um numero por 0");
+            } finally {
+                System.out.println("Chegou no finally");
+            }
+        }while (continuaLooping);
+
+
+        System.out.println("O codigo continua");
+
+    }
+}
